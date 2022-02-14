2dc30ad3b7c3fb238125646d9a7aeb52c192fdab
Nabeel Ahmed <nabeel3353045805@hotmail.com>
2022-02-14 17:51:08 +0500

    ThumbsDown gesture added

--- src/gestures/index.js
+++ src/gestures/index.js
@@ -1,7 +1,9 @@
 import VictoryGesture from './Victory';
 import ThumbsUpGesture from './ThumbsUp';
+import ThumbsDownGesture from './ThumbsDown';
 
 export {
   VictoryGesture,
   ThumbsUpGesture,
+  ThumbsDownGesture
 }
